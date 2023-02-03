8d0940295792cd159fe74a4beb936be7eea97306
Igor Tiburcio Cavalcanti <igor.tiburcio.cs@gmail.com>
2023-02-02 21:12:14 -0300

    configured tailwind and tailwind-Styled-Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
+import { Global } from './styles/globalStyle';
 
 export default  function App() {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <Global >
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </Global>
   );
 }
---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,11 @@
+import tw from 'tailwind-styled-components';
+
+export const Global = tw.div`
+bg-slate-600
+p-0
+m-0
+w-full
+h-full
+grid
+grid-cols-12
+`;
